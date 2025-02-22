7bec92324b1b67e6d01fd899490796e155c6c3dc
priestprog <abagrov04@gmail.com>
2025-02-22 14:17:36 +0300

    Add a feature: print tree of last commit

--- 20250217/1/task_1.py
+++ 20250217/1/task_1.py
@@ -1,8 +1,10 @@
 from pathlib import Path
 import sys
 import zlib
+from glob import iglob
+from os.path import basename, dirname
 
-
+# функция при вызове без второго аргумента
 def list_branches(repo_path):
     repo = Path(f"{repo_path}/.git/refs/heads")
 
@@ -13,7 +15,7 @@ def list_branches(repo_path):
     for item in repo.iterdir():
         print(item.name)
 
-
+# Функция при вызове с вторым аргументом
 def check_branch(repo_path, branch_name):
     with open(f"{repo_path}/.git/refs/heads/{branch_name}", "r") as f:
         commit = f.read()
@@ -23,8 +25,45 @@ def check_branch(repo_path, branch_name):
 
     decompressed_message = zlib.decompress(message)
     obj_type, _, content = decompressed_message.partition(b"\x00")
-    print(content.decode())
 
+    print(content.decode())
+    #Печать последенего коммита
+
+
+    tree = content.decode().split("\n")[0].split(" ")[1]
+    with open(f"{repo_path}/.git/objects/{tree[:2]}/{tree[2:]}", "rb") as f3:
+        tree_content = f3.read()
+
+    branches = zlib.decompress(tree_content)
+    obj_type, _, data = branches.partition(b"\x00")
+
+    index = 0
+    result = []
+    git_modes = {
+        "100644": "blob",
+        "40000": "tree"
+    }
+
+    while index < len(data):
+        mode_end = data.find(b" ", index)
+        mode = data[index:mode_end].decode()
+        index = mode_end + 1
+
+        name_end = data.find(b"\x00", index)
+        name = data[index:name_end].decode()
+        index = name_end + 1
+
+        sha1 = data[index:index + 20]
+        index += 20
+
+        sha1_hex = sha1.hex()
+
+        result.append(f"{git_modes.get(mode, "Неизвестный тип")} {sha1_hex}    {name} ")
+
+    print("\n".join(result))
+    # Печать содержимого tree на который указывает последний коммит
+
+    
 
 
 if len(sys.argv) < 2:
