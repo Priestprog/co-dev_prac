b713f9f7cb86fb20ddc5c657652afec2b6bdd39c
priestprog <abagrov04@gmail.com>
2025-04-14 14:23:42 +0300

    Done of classwork 14.04

--- 20250414/0/counter.py
+++ 20250414/0/counter.py
@@ -1,9 +1,27 @@
+import gettext
+import os
+import locale
+
+_podir = os.path.join(os.path.dirname(__file__), "po")
+translation = gettext.translation("messages", _podir, fallback=True)
+_, ngettext = translation.gettext, translation.ngettext
+
+LOCALES = {
+    ("ru_RU", "UTF-8"): gettext.translation("messages", "po", ["ru"]),
+    ("en_US", "UTF-8"): gettext.NullTranslations(),
+}
+
+def _(text):
+    return LOCALES[locale.getlocale()].gettext(text)
+
 while True:
-    try:
-        line = input()
-        words = line.split()
-        count = len(words)
-        print(f"Entered {count} word(s)")
-    except KeyboardInterrupt:
-        print("\nEXIT")
-        break
+        try:
+            line = input()
+            for loc in LOCALES:
+                locale.setlocale(locale.LC_ALL, loc)
+                words = line.split()
+                count = len(words)
+                print(_("Entered"), ngettext("{} word", "{} words", count).format(count))
+        except KeyboardInterrupt:
+            print("\nEXIT")
+            break
