e31ab6ef6a4c3bfd80564d6ba25babd55c5ce469
priestprog <abagrov04@gmail.com>
2025-04-30 21:54:53 +0300

    intermediate result: Test, doc without translation. TODO: i18n

--- 20250428/1/dodo.py
+++ 20250428/1/dodo.py
@@ -5,59 +5,6 @@ from zipfile import ZipFile
 DOIT_CONFIG = {"default_tasks": ["html"]}
 
 
-def task_docs():
-    """Генерация документации Sphinx (html + text)"""
-    rst_files = glob.glob('docs/*.rst')
-    return {
-        'actions': [
-            "sphinx-build -b html docs docs/_build/html",
-            "sphinx-build -b text docs docs/_build/text",
-        ],
-        'file_dep': ['docs/conf.py'] + rst_files,
-        'targets': ['docs/_build/html/index.html', 'docs/_build/text/index.txt'],
-        'clean': True,
-    }
-
-
-def task_erase():
-    """Удаление всех сборок (вручную сделай коммит перед этим!)"""
-    def erase():
-        shutil.rmtree('docs/_build', ignore_errors=True)
-        try:
-            os.remove('docs.zip')
-            os.remove('docs.list')
-        except FileNotFoundError:
-            pass
-    return {
-        'actions': [erase],
-        'verbosity': 2,
-    }
-
-
-def task_zip():
-    """Создание архива документации"""
-    return {
-        'actions': ['cd docs/_build/html && zip -r ../../../docs.zip .'],
-        'file_dep': ['docs/_build/html/index.html'],
-        'targets': ['docs.zip'],
-        'clean': True,
-    }
-
-
-def task_stat():
-    """Создание списка содержимого zip-архива"""
-    def generate_list():
-        with ZipFile('docs.zip') as zf:
-            with open('docs.list', 'w') as f:
-                f.write('\n'.join(zf.namelist()))
-    return {
-        'actions': [generate_list],
-        'file_dep': ['docs.zip'],
-        'targets': ['docs.list'],
-        'clean': True,
-    }
-
-
 def task_pot():
     """Генерация .pot шаблона"""
     return {
@@ -87,6 +34,16 @@ def task_mo():
     }
 
 
+
+
+
+
+
+
+
+
+
+
 def task_i18n():
     """Полная генерация переводов"""
     return {
@@ -94,23 +51,34 @@ def task_i18n():
         'task_dep': ['pot', 'po', 'mo'],
     }
 
-
 def task_html():
     """Генерация HTML-документации с учётом перевода"""
+    rst_files = glob.glob('source/*.rst')
     return {
-        'actions': ['sphinx-build -b html docs docs/_build/html'],
-        'file_dep': ['docs/conf.py'] + glob.glob('docs/*.rst'),
-        'targets': ['docs/_build/html/index.html'],
+        'actions': ['sphinx-build -b html source build/html'],
+        'file_dep': ['source/conf.py'] + rst_files,
+        'targets': ['build/html/index.html'],
         'task_dep': ['i18n'],
-        'clean': [(shutil.rmtree, ['docs/_build/html'], {'ignore_errors': True})],
+        'clean': [(shutil.rmtree, ['build/html'], {'ignore_errors': True})],
     }
 
+def task_docs():
+    """Генерация text-документации Sphinx (без перевода)"""
+    rst_files = glob.glob('source/*.rst')
+    return {
+        'actions': ["sphinx-build -b html source build/html"],
+        'file_dep': ['source/conf.py'] + rst_files,
+        'targets': ['build/html/index.html'],
+        'clean': [(shutil.rmtree, ['build/html'], {'ignore_errors': True})],
+    }
 
 def task_test():
     """Прогон тестов клиента и сервера"""
     return {
-        'actions': ['pytest tests/'],
-        'task_dep': ['i18n'],
-        'clean': True,
+        'actions': [
+            'python3 -m unittest server_test.py',
+            'python3 -m unittest client_test.py',
+        ],
     }
+
 
